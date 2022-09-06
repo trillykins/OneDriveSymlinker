db78a3151f38fd18a04742c4cd713404e359dfe7
Kagemand Andersen <trillykins@gmail.com>
2022-09-06 09:28:43 +0200

    More functionality
    Can move save files to onedrive and create symlink or just create symlink if save files are already in onedrive

--- OneDriveSymlinker/OneDriveSymlinker.Designer.cs
+++ OneDriveSymlinker/OneDriveSymlinker.Designer.cs
@@ -34,11 +34,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 95);
+            this.button1.Location = new System.Drawing.Point(12, 136);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(143, 23);
             this.button1.TabIndex = 0;
@@ -49,26 +50,26 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(161, 99);
+            this.label1.Location = new System.Drawing.Point(161, 140);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.Size = new System.Drawing.Size(94, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "path";
+            this.label1.Text = "User profile path";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(161, 194);
+            this.label2.Location = new System.Drawing.Point(161, 206);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(117, 15);
+            this.label2.Size = new System.Drawing.Size(83, 15);
             this.label2.TabIndex = 2;
-            this.label2.Text = "target OneDrive path";
+            this.label2.Text = "OneDrive path";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 190);
+            this.button2.Location = new System.Drawing.Point(12, 202);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(143, 23);
             this.button2.TabIndex = 3;
@@ -89,7 +90,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(12, 26);
+            this.checkBox1.Location = new System.Drawing.Point(12, 73);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(284, 19);
             this.checkBox1.TabIndex = 5;
@@ -97,11 +98,22 @@
             this.checkBox1.UseVisualStyleBackColor = true;
             this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(389, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "This will create symlink from save files already in OneDrive to user profile";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
             // OneDriveSymlinker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(485, 313);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -123,5 +135,6 @@
         private Button button2;
         private Button button3;
         private CheckBox checkBox1;
+        private Label label3;
     }
 }
--- OneDriveSymlinker/OneDriveSymlinker.cs
+++ OneDriveSymlinker/OneDriveSymlinker.cs
@@ -2,28 +2,29 @@ namespace WinFormsApp1
 {
     public partial class OneDriveSymlinker : Form
     {
-        private string path = string.Empty;
-        private string targetPath = string.Empty;
+        private string userPath = string.Empty;
+        private string oneDrivePath = string.Empty;
         private string username = string.Empty;
         private bool moveSaveFilesToOneDrive = false;
 
         public OneDriveSymlinker()
         {
             InitializeComponent();
-            Text = "OneDriveSymlinker";
+            Text = nameof(OneDriveSymlinker);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             username = Environment.UserName;
-            path = $"C:\\Users\\{username}\\Documents\\";
-            targetPath = $"C:\\Users\\{username}\\OneDrive";
-            label1.Text = path;
-            label2.Text = targetPath;
+            userPath = $"C:\\Users\\{username}\\Documents";
+            oneDrivePath = $"C:\\Users\\{username}\\OneDrive";
+            label1.Text = userPath;
+            label2.Text = oneDrivePath;
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
+
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -33,18 +34,18 @@ namespace WinFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            var result = DirectoryPicker(path);
+            var result = DirectoryPicker(userPath);
             if (string.IsNullOrEmpty(result)) return;
-            path = result;
-            label1.Text = path;
+            userPath = result;
+            label1.Text = userPath;
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            var result = DirectoryPicker(targetPath);
+            var result = DirectoryPicker(oneDrivePath);
             if (string.IsNullOrEmpty(result)) return;
-            targetPath = result;
-            label2.Text = targetPath;
+            oneDrivePath = result;
+            label2.Text = oneDrivePath;
         }
 
         private string DirectoryPicker(string? initialPath = null)
@@ -66,33 +67,60 @@ namespace WinFormsApp1
         {
             if (moveSaveFilesToOneDrive)
             {
-                // Directory.Move(string, string)
-                // CreateSymbolicLink(string, string)
-                // Verify shit
+                FileInfo dir = new FileInfo(userPath);
+                oneDrivePath = $"{oneDrivePath}\\{dir.Name}";
+                if (!Directory.Exists(userPath))
+                {
+                    MessageBox.Show("Save folder not found in user profile path!");
+                    return;
+                }
+                if (Directory.Exists(oneDrivePath))
+                {
+                    MessageBox.Show("Folder already exists in OneDrive!");
+                    return;
+                }
+
+                Directory.Move(userPath, oneDrivePath);
             }
             else
             {
-                // CreateSymbolicLink(string, string)
-                // Verify shit
+                FileInfo dir = new FileInfo(oneDrivePath);
+                userPath = $"{userPath}\\{dir.Name}";
+                if (Directory.Exists(userPath))
+                {
+                    MessageBox.Show("Save folder already exists in user profile path!");
+                    return;
+                }
+                if (!Directory.Exists(oneDrivePath))
+                {
+                    MessageBox.Show("Save folder does not exist in OneDrive!");
+                    return;
+                }
             }
-            FileInfo dir = new FileInfo(targetPath);
-            path = $"{path}{dir.Name}";
-            if (Directory.Exists(path))
-            {
-                MessageBox.Show($"Symlink already exists!");
-                return;
-            }
-            var result = Directory.CreateSymbolicLink(path, targetPath);
-            if (!(result.Exists && Directory.GetFiles(result.FullName).Length == Directory.GetFiles(targetPath).Length))
+            var result = Directory.CreateSymbolicLink(userPath, oneDrivePath);
+            if (!(result.Exists && Directory.GetFiles(result.FullName).Length == Directory.GetFiles(oneDrivePath).Length))
             {
                 throw new Exception("Symlink botched!");
             }
-            MessageBox.Show($"Symlink from {targetPath} to {path} successfully created!");
+            MessageBox.Show($"Symlink from {oneDrivePath} to {userPath} successfully created!");
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             moveSaveFilesToOneDrive = checkBox1.Checked;
+            if (moveSaveFilesToOneDrive)
+            {
+                label3.Text = "This will move save files from user profile to OneDrive and create symlink";
+            }
+            else
+            {
+                label3.Text = "This will create symlink from save files already in OneDrive to user profile";
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
